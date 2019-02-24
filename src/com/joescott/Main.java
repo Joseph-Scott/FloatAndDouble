cc86c02bd1ac507b3cc8083edf26e42e7a3e52b7
Joseph Scott <jbs198585@gmail.com>
2019-02-24 17:04:12 -0500

    floatanddouble

--- src/com/joescott/Main.java
+++ src/com/joescott/Main.java
@@ -8,7 +8,7 @@ public class Main {
         // width of float = 32 (4 bytes)
         float myFloatValue = 5f / 3f;
         // width of double = 64 (8 bytes)
-        double myDoubleValue = 5d / 2d;
+        double myDoubleValue = 5d / 3d;
         //
         System.out.println("myIntValue = " + myIntValue);
         System.out.println("myFloatValue = " + myFloatValue);
