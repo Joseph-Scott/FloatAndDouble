b3d0cc85080a2490f7687c214ad4a5cf38a01132
Joseph Scott <jbs198585@gmail.com>
2019-02-24 17:29:37 -0500

    pounds to kilograms

--- src/com/joescott/Main.java
+++ src/com/joescott/Main.java
@@ -13,5 +13,17 @@ public class Main {
         System.out.println("myIntValue = " + myIntValue);
         System.out.println("myFloatValue = " + myFloatValue);
         System.out.println("myDoubleValue = " + myDoubleValue);
+
+        // Convert a given number of pounds to kilograms
+        // 1. Create a variable to store the number of pounds
+        // 2. Calculate the number of Kilograms for the number above and store in a variable.
+        // 3. Print out the result.
+        //
+        // NOTES: 1 pound is equal to 0.45359237 kilograms.
+
+        double myPounds = 2d;
+        double myKilograms = myPounds * 0.45359227d;
+
+        System.out.println("Kilograms = " + myKilograms);
     }
 }
